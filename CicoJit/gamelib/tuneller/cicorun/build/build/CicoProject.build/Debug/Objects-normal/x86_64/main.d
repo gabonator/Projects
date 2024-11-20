af24dd35f35c9c13f6ecbc2d03bcd05414316a96
Gabriel Valky <gabriel@valky.eu>
2024-11-21 00:25:57 +0100

    tuneller cleanup

--- CicoJit/gamelib/tuneller/cicorun/build/build/CicoProject.build/Debug/Objects-normal/x86_64/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/gabrielvalky/Documents/git/Projects/CicoJit/gamelib/tuneller/dos/cicorun/main.cpp \
-  /Users/gabrielvalky/Documents/git/Projects/CicoJit/gamelib/tuneller/dos/cicorun/sdl.h \
-  /Users/gabrielvalky/Documents/git/Projects/CicoJit/gamelib/tuneller/dos/cicorun/cicoemu.h \
-  /Users/gabrielvalky/Documents/git/Projects/CicoJit/gamelib/tuneller/dos/cicorun/ega.h \
-  /Users/gabrielvalky/Documents/git/Projects/CicoJit/gamelib/tuneller/dos/cicorun/controls.h
--- CicoJit/gamelib/tuneller/cicorun/build/build/CicoProject.build/Debug/Objects-normal/x86_64/tunneler.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/gabrielvalky/Documents/git/Projects/CicoJit/gamelib/tuneller/dos/cicorun/tunneler.cpp \
-  /Users/gabrielvalky/Documents/git/Projects/CicoJit/gamelib/tuneller/dos/cicorun/cicoemu.h
