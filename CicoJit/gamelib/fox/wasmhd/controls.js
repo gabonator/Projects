20956c63ccc51cc7ff5b7408a1b3edd282938320
Gabriel Valky <gabriel@valky.eu>
2025-01-10 00:41:33 +0100

    fox sounds and music

--- CicoJit/gamelib/fox/wasmhd/controls.js
+++ CicoJit/gamelib/fox/wasmhd/controls.js
@@ -1,4 +1,5 @@
 document.onkeydown = evt => {
+  setInteracted();
   if (onKey(evt.keyCode, 1))
     evt.preventDefault();
 }
@@ -6,10 +7,21 @@ document.onkeyup = evt => {
   if (onKey(evt.keyCode, 0))
     evt.preventDefault();
 }
+document.onclick = () => setInteracted();
 
 function onKey(code, pressed)
 {
   var cbit = (seg, ofs, e) => game.memoryBuffer[seg*16+ofs] = e;
+
+  if (curintro && pressed)
+    return;
+  if (curintro)
+  {
+    curintro = false;
+    document.querySelector("#intro").style.display = "none";
+    playSong("music/info.mp3");
+  }
+
   switch (code)                     
   {
     case 38: cbit(0x168f, 0x488, pressed); break;
@@ -18,8 +30,6 @@ function onKey(code, pressed)
     case 39: cbit(0x168f, 0x48d, pressed); break;
     case 32: cbit(0x168f, 0x45c, pressed); 
              cbit(0x168f, 0x042b, pressed); 
-             if (pressed)
-               document.querySelector("#intro").style.display = "none";
     break;
     case 65: 
       document.querySelectorAll('.layer').forEach(div => {
