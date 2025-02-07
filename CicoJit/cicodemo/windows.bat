7ddf25dfbd3ddecf531e21f08b94ce2d59d479f6
Gabriel Valky <gabriel@valky.eu>
2025-02-08 00:55:33 +0100

    emscripten windows build

--- CicoJit/cicodemo/windows.bat
+++ CicoJit/cicodemo/windows.bat
@@ -73,3 +73,58 @@ if not exist bin/goose.exe (
 
 cd bin
 goose.exe
+cd ..
+
+echo Starting webassembly build
+
+if not exist temppython (
+  curl https://www.python.org/ftp/python/3.9.2/python-3.9.2-embed-win32.zip -o download/python.zip
+  mkdir temppython
+  cd temppython
+  tar xvzf ../download/python.zip
+  cd ..
+)
+
+if not exist emsdk-4.0.1 (
+  set PATH=%~dp0\temppython;%PATH%
+  curl -L https://github.com/emscripten-core/emsdk/archive/refs/tags/4.0.1.zip -o download/emcc.zip
+  tar xvzf download/emcc.zip 
+  cd emsdk-4.0.1
+  call emsdk install latest
+  cd ..
+)
+
+if not exist bin\inline.js (
+  echo var fs = require(^"fs^" ^); > bin\encode.js
+  echo var prefix = 'Module = typeof(Module ^) != ^"undefined^" ? Module : ^{^}; Module[^"' + process.argv[3] + '^"] = (( ^) =^> ^{ var d=^"'; >> bin\encode.js
+  echo var suffix = '^"; return Uint8Array.from(atob(d ^), c =^> c.charCodeAt(0 ^) ^^ 71 ^);^} ^)( ^);'; >> bin\encode.js
+  echo console.log(prefix + fs.readFileSync(process.argv[2] ^).map(x=^>x ^^ 71 ^).toString(^"base64^" ^) + suffix ^); >> bin\encode.js
+)
+
+if not exist wasm (
+  mkdir wasm
+  mkdir wasm\temp
+  curl https://raw.githubusercontent.com/gabonator/Projects/refs/heads/master/CicoJit/gamelib/goose/wasm/wasmapp.js -o wasm/wasmapp.js
+  curl https://raw.githubusercontent.com/gabonator/Projects/refs/heads/master/CicoJit/gamelib/goose/wasm/controls.js -o wasm/controls.js
+  curl https://raw.githubusercontent.com/gabonator/Projects/refs/heads/master/CicoJit/gamelib/goose/wasm/goose.html -o wasm/goose.html
+
+  set EMSDK_QUIET=1
+  emsdk-4.0.1\emsdk activate latest
+  cd wasm\temp
+  emcc ..\..\port\goose.cpp ..\..\port\wasmhost.cpp -gsource-map -O0 --std=c++11 -s TOTAL_STACK=1024 -s TOTAL_MEMORY=2097152 -s MINIMAL_RUNTIME=1 -s WASM=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s WARN_ON_UNDEFINED_SYMBOLS=0 -s ASSERTIONS=1 -sASYNCIFY -s EXPORTED_FUNCTIONS="['_appLoop', '_appInit', '_appFinish', '_appMemory', '_appBlit', '_appVideo', '_asyncifyBuffer']" -o out.js 
+  cd ..\..
+
+  node bin\encode.js wasm\temp\out.wasm wasmBinary > wasm/resources.js
+  node bin\encode.js dos\GOOSE.EXE goose.exe >> wasm/resources.js
+  node bin\encode.js dos\BIRD1.X bird1.x >> wasm/resources.js
+  node bin\encode.js dos\BIRD2.X bird2.x >> wasm/resources.js
+  node bin\encode.js dos\BLOX.X blox.x >> wasm/resources.js
+  node bin\encode.js dos\INTRO.X intro.x >> wasm/resources.js
+  node bin\encode.js dos\NEWBIRD.X newbird.x >> wasm/resources.js
+  node bin\encode.js dos\PODZ1.X podz1.x >> wasm/resources.js
+
+  start wasm\goose.html
+  rem for some reason it won't continue after exiting this function
+)
+
+start wasm\goose.html
